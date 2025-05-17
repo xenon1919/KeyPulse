5d767d2ccfc0a5004c6024045b94d400fb0b99ea
Ramanchi Rishi Sai Teja <102644611+xenon1919@users.noreply.github.com>
2025-05-17 22:36:41 +0530

    Update Login.jsx

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -20,13 +20,16 @@ const Login = () => {
       return;
     }
     try {
-      const response = await fetch("https://keypulse-backend.onrender.com/api/auth/login", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({ username, password }),
-      });
+      const response = await fetch(
+        "https://keypulse-backend.onrender.com/api/auth/login",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ username, password }),
+        }
+      );
       if (response.ok) {
         const { token } = await response.json();
         localStorage.setItem("token", token);
